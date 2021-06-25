4b5bbb3679b854cb0cb3c501a346e0c245925a5b
Mateo C <m.cordoba17@ciaf.edu.co>
2021-06-24 20:10:44 -0500

    cambio texto -1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Mundo!
+          Hola Mundo! Saluditos...
         </p>
         <a
           className="App-link"
